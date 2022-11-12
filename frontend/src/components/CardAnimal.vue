0325601d4c17c806ab9163aafa1f9402f9760218
Ratchaphon1412 <ratchaphon.h111@gmail.com>
2022-11-12 21:10:16 +0700

    fix error image

--- frontend/src/components/CardAnimal.vue
+++ frontend/src/components/CardAnimal.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="card" style="width: 18rem;">
+  <div class="card" style="width: 18rem">
     <img
-        src="@/assets/Nueng.jpg"
-        width="50%"
-        height="350"
-        class="card-img-top"
-        alt="..."
+      src="@/assets/nueng.png"
+      width="50%"
+      height="350"
+      class="card-img-top"
+      alt="..."
     />
     <div class="card-body">
       <h5 class="card-title">Card title</h5>
       <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <p class="card-text">
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </p>
     </div>
   </div>
 </template>
@@ -19,17 +22,17 @@
 export default {
   data() {
     return {
-      animal: null
-    }
+      animal: null,
+    };
   },
   props: {
     animal: Object,
     url: {
       type: String,
-      default: '',
+      default: "",
     },
-  }
-}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
