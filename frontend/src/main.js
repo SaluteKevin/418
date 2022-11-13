1f21f395cdee93f3c3bd5f0ee8c3dc089fc621dd
Ratchaphon1412 <ratchaphon.h111@gmail.com>
2022-11-13 12:23:59 +0700

    change vuex to pinia

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,14 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
-import store from './store'
+// import store from './store'
     import "bootstrap/dist/css/bootstrap.css";
     import "bootstrap/dist/js/bootstrap.js";
     import "bootstrap-icons/font/bootstrap-icons.css";
 import "./style/style.css";
 import "three/build/three.js";
+import { createPinia } from 'pinia';
 
 import { TroisJSVuePlugin } from 'troisjs';
 
 
-createApp(App).use(store).use(router).use(TroisJSVuePlugin).mount('#app')
+// createApp(App).use(store).use(router).use(TroisJSVuePlugin).use(createPinia()).mount('#app')
+createApp(App).use(router).use(TroisJSVuePlugin).use(createPinia()).mount('#app')
